4fa37bc7d167f08ef0e0f7125d30b5a44645098e
Ashish Ranjan <ashish.ranjan@coverfoxmail.com>
2020-02-02 00:47:37 +0530

    route setup and project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-const App = () => {
+const App = ({msg}: {msg: string}) => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to {msg}.
         </p>
         <a
           className="App-link"
@@ -20,7 +20,7 @@ const App = () => {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import { Router, Route } from 'react-router'
+import { createBrowserHistory } from "history"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const history = createBrowserHistory()
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <Router history={history}>
+        <Route
+            key="home"
+            path="/post/:postId"
+            component={() => <App msg="reload"/>}
+        />
+        <Route
+            key="post"
+            path="/"
+            component={() => <App msg="asfaf"/>}
+        />
+    </Router>, 
+    document.getElementById('root')
+)
